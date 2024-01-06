5373804fda188fe705f92dff9cae9460cae06b58
Mayank Upadhyay <140595877+Mayankupadhyay-25@users.noreply.github.com>
2024-01-06 23:17:39 +0530

    Images type changed By Ganesh

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const press = ()=>{
     var image = document.querySelector("#imageId")
     var headingEle = document.getElementById("declaration") 
     switch(randomX){
-        case 1 : image.setAttribute("src","images/1.svg")
+        case 1 : image.setAttribute("src","images/1.png")
                  headingEle.innerHTML = "koi nhi sbka smay aata hai"   
                  break
-        case 2 : image.setAttribute("src","images/2.svg")
+        case 2 : image.setAttribute("src","images/2.png")
                  headingEle.innerHTML = "o hoo"   
                  break          
-        case 3 : image.setAttribute("src","images/3.svg")
+        case 3 : image.setAttribute("src","images/3.png")
                  headingEle.innerHTML = " itna chalta hai yaar"   
                  break
-        case 4 : image.setAttribute("src","images/4.svg")
+        case 4 : image.setAttribute("src","images/4.png")
                  headingEle.innerHTML = "thik hi hai"   
                  break                 
-        case 5 : image.setAttribute("src","images/5.svg")
+        case 5 : image.setAttribute("src","images/5.png")
                  headingEle.innerHTML = "o triii thoda sa reh gya yaar!!!"   
                  break
-        case 6 : image.setAttribute("src","images/6.svg")
+        case 6 : image.setAttribute("src","images/6.png")
                  headingEle.innerHTML = "kataii swad aa gya!!!"   
                  break
         default: headingEle.innerHTML = "Something wrong happened"            
