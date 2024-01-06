3155abc5fde317591a5d347b0748a6ec108bfcea
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2024-01-06 22:51:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const press = ()=>{
     var image = document.querySelector("#imageId")
     var headingEle = document.getElementById("declaration") 
     switch(randomX){
-        case 1 : image.setAttribute("src","diceroll/diceImages/1.svg")
+        case 1 : image.setAttribute("src","1.svg")
                  headingEle.innerHTML = "koi nhi sbka smay aata hai"   
                  break
-        case 2 : image.setAttribute("src","diceroll/diceImages/2.svg")
+        case 2 : image.setAttribute("src","2.svg")
                  headingEle.innerHTML = "o hoo"   
                  break          
-        case 3 : image.setAttribute("src","diceroll/diceImages/3.svg")
+        case 3 : image.setAttribute("src","3.svg")
                  headingEle.innerHTML = " itna chalta hai yaar"   
                  break
-        case 4 : image.setAttribute("src","diceroll/diceImages/4.svg")
+        case 4 : image.setAttribute("src","4.svg")
                  headingEle.innerHTML = "thik hi hai"   
                  break                 
-        case 5 : image.setAttribute("src","diceroll/diceImages/5.svg")
+        case 5 : image.setAttribute("src","5.svg")
                  headingEle.innerHTML = "o triii thoda sa reh gya yaar!!!"   
                  break
-        case 6 : image.setAttribute("src","diceroll/diceImages/6.svg")
+        case 6 : image.setAttribute("src","6.svg")
                  headingEle.innerHTML = "kataii swad aa gya!!!"   
                  break
         default: headingEle.innerHTML = "Something wrong happened"            
